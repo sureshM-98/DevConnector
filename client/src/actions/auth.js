fe6553911c34c49591972098789f5537cf393465
Suresh M <suresh70198@gmail.com>
2021-06-12 22:18:40 +0530

    DevConnecter

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import setAuthToken from '../utility/setAuthToken';
 import { setAlert } from './alert';
-import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT } from './types';
+import { REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT, CLEAR_PROFILE } from './types';
 
 // Load user
 export const loadUser = () => async dispatch => {
@@ -87,5 +87,6 @@ export const login = (email, password) => async dispatch => {
 
 // Logout User
 export const logout = () => dispatch => {
+  dispatch({ type: CLEAR_PROFILE });
   dispatch({ type: LOGOUT });
 };
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,9 +8,13 @@ const Navbar = ({ auth: { isAuthenticated, loading }, logout }) => {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" /> <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} to="!#">
-          <i className="fas fa-sign-out-alt"></i>
-          <span className="hide-sm">Logout</span>
+          <i className="fas fa-sign-out-alt" /> <span className="hide-sm">Logout</span>
         </a>
       </li>
     </ul>
