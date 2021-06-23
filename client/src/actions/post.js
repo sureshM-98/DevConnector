5730e22dd7a08c8337ae3630159bf597795c1c0f
Suresh M <suresh70198@gmail.com>
2021-06-23 22:41:47 +0530

    DevConnector

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { setAlert } from './alert';
-import { GET_POSTS, POST_ERROR } from './types';
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from './types';
 
 // Get posts
 export const getPosts = () => async dispatch => {
@@ -9,12 +9,65 @@ export const getPosts = () => async dispatch => {
 
     dispatch({
       type: GET_POSTS,
-      payload: res.data
+      payload: res.data,
     });
   } catch (err) {
     dispatch({
       type: POST_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status }
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Likes
+export const addLike = id => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/like/${id}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { id, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Remove Likes
+export const removeLike = postId => async dispatch => {
+  try {
+    const res = await axios.put(`/api/posts/unlike/${postId}`);
+
+    dispatch({
+      type: UPDATE_LIKES,
+      payload: { postId, likes: res.data },
+    });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Remove Likes
+export const deletePost = postId => async dispatch => {
+  try {
+    await axios.delete(`/api/posts/${postId}`);
+
+    dispatch({
+      type: DELETE_POST,
+      payload: postId,
+    });
+
+    dispatch(setAlert('Post Removed', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
     });
   }
 };
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,29 +1,41 @@
-import {GET_POSTS, POST_ERROR} from '../actions/types'
+import { DELETE_POST, GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
 
 const initialState = {
   posts: [],
   post: null,
   loading: true,
-  error: {}
-}
+  error: {},
+};
 
-export default function(state = initialState, action){
-  const {type, payload} = action;
+export default function (state = initialState, action) {
+  const { type, payload } = action;
 
-  switch(type){
+  switch (type) {
     case GET_POSTS:
-      return{
+      return {
         ...state,
         posts: payload,
-        loading: false
-      }
-      case POST_ERROR:
-      return{
+        loading: false,
+      };
+    case DELETE_POST:
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== payload),
+        loading: false,
+      };
+    case POST_ERROR:
+      return {
         ...state,
         error: payload,
-        loading: false
-      }
-      default:
-        return state;
+        loading: false,
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post => (post._id === payload.id ? { ...post, likes: payload.likes } : post)),
+        loading: false,
+      };
+    default:
+      return state;
   }
 }
